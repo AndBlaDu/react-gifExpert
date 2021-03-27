4ba9b6dab87df544cacc72d3b2bec86c5069f4e7
Andres Blanco <andresblanco03.ab@gmail.com>
2021-03-26 19:52:18 -0600

    Docs

---
+++ docs/precache-manifest.991dccd6677390df921466a2503b4371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfefbaa0245baae2d60909bac8eb6863",
+    "url": "/index.html"
+  },
+  {
+    "revision": "716429740c0308fbd305",
+    "url": "/static/css/main.70fa9018.chunk.css"
+  },
+  {
+    "revision": "01beeba6332dad5debed",
+    "url": "/static/js/2.4d28b388.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.4d28b388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716429740c0308fbd305",
+    "url": "/static/js/main.a6292edd.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
